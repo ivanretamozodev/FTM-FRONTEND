670ef7cf8379cbc27cb8ea00a30d2d389e4e971f
ivan retamozo <retamozoi445@gmail.com>
2023-01-13 13:55:22 -0300

    cambios en navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,21 @@ import { CommonModule } from "@angular/common";
 import { CardComponent } from "./components/card/card.component";
 import { SmBannerComponent } from "./components/sm-banner/sm-banner.component";
 import { SideBarComponent } from "./components/side-bar/side-bar.component";
+import { SearchBarComponent } from "./components/search-bar/search-bar.component";
 
 @NgModule({
-  declarations: [CardComponent, SmBannerComponent, SideBarComponent],
+  declarations: [
+    CardComponent,
+    SmBannerComponent,
+    SideBarComponent,
+    SearchBarComponent,
+  ],
   imports: [CommonModule],
-  exports: [CardComponent, SmBannerComponent, SideBarComponent],
+  exports: [
+    CardComponent,
+    SmBannerComponent,
+    SideBarComponent,
+    SearchBarComponent,
+  ],
 })
 export class SharedModule {}
