4868aeef0ad48a792a995926d03e64b1fb4405a2
ivan retamozo <retamozoi445@gmail.com>
2023-01-29 17:22:27 -0300

    color principal final

--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -1,38 +1,34 @@
-import { catchError } from "rxjs";
-import { AuthService } from "./../../../services/auth/auth.service";
-import { Router } from "@angular/router";
-import { Validators } from "@angular/forms";
-import { FormGroup } from "@angular/forms";
-import { FormBuilder } from "@angular/forms";
-import { Component } from "@angular/core";
+import { catchError } from 'rxjs';
+import { AuthService } from './../../../services/auth/auth.service';
+import { Router } from '@angular/router';
+import { Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-register",
-  templateUrl: "./register.component.html",
-  styleUrls: ["./register.component.scss"],
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-  registerForm: FormGroup = this.fb.group({
-    name: ["magui", [Validators.required]],
-    email: ["magui@test.com", [(Validators.required, Validators.email)]],
-    password: ["123456", [(Validators.required, Validators.minLength(6))]],
-  });
-  constructor(
-    private fb: FormBuilder,
-    private router: Router,
-    private authService: AuthService
-  ) {}
-
-  registerError: boolean = false;
-
-  register = () => {
-    const { email, password, name } = this.registerForm.value;
-    this.authService.register(email, password, name).subscribe((auth) => {
-      if (auth) {
-        this.router.navigateByUrl("/home");
-      } else {
-        this.registerError = true;
-      }
+    registerForm: FormGroup = this.fb.group({
+        name: ['', [Validators.required]],
+        email: ['', [(Validators.required, Validators.email)]],
+        password: ['', [(Validators.required, Validators.minLength(6))]],
     });
-  };
+    constructor(private fb: FormBuilder, private router: Router, private authService: AuthService) {}
+
+    registerError: boolean = false;
+
+    register = () => {
+        const { email, password, name } = this.registerForm.value;
+        this.authService.register(email, password, name).subscribe((auth) => {
+            if (auth) {
+                this.router.navigateByUrl('/home');
+            } else {
+                this.registerError = true;
+            }
+        });
+    };
 }
